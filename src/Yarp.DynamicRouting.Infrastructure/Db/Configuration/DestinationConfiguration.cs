db401f579224d2389b33b4e4c52f7b3f4c2ccd84
knyL <linhleduc.it@hotmail.com>
2023-05-14 20:06:15 +0700

    feat: implement pg context

---
+++ src/Yarp.DynamicRouting.Infrastructure/Db/Configuration/DestinationConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Newtonsoft.Json;
+using Yarp.DynamicRouting.Core.Entities;
+
+namespace Yarp.DynamicRouting.Infrastructure.Db.Configuration;
+
+public class DestinationConfiguration : IEntityTypeConfiguration<Destination>
+{
+    public void Configure(EntityTypeBuilder<Destination> builder)
+    {
+        builder.ToTable(PgTables.Destination).HasKey(e => e.Id);
+        builder.Property(e => e.Metadata)
+               .HasConversion(
+                   v => JsonConvert.SerializeObject(v, Formatting.Indented),
+                   v => JsonConvert.DeserializeObject<List<KeyValueItem>>(v));
+
+    }
+}
+
